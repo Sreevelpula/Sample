8de7fea7ed0232bfca9e8e528efcc087555a90bc
sr6h8x6 <sriharsha_ganti@gap.com>
2016-03-22 21:41:21 -0700

    Ruby implementation for sorting array

---
+++ sort_array/sort_array.rb
@@ -0,0 +1,20 @@
+input_array = [5,1,'z','b','s',3,2,7,'a','c']
+
+def swap(array)
+  i = 0
+  while(i<array.length)
+    j = i+1
+    while(j<array.length)
+
+    if(array[i].to_s > array[j].to_s)
+      array[i],array[j] = array[j],array[i]
+
+    end
+    j = j+1
+    end
+    i = i+1
+  end
+  return array.reverse
+end
+
+p swap(input_array)
