169693c00a537a7cfe409d1b0efeabca8c75300f
Sreevelpula <srelathat@gmail.com>
2017-06-23 18:51:49 -0700

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,22 @@
+
+public class Test1 {
+	
+	public static void main(String [] args){
+
+		
+		String a = "H";
+		String b = "e";
+		String c = "l";
+		String d = "l";
+		String e = "o";
+		String f = "W";
+		String g = "o";
+		String h = "r";
+		String i = "l";
+		String j = "d";		
+		
+	System.out.println(a+b+c+d+e+" "+f+g+h+i+j);	
+	
+	
+	}
+}
